20c0818397ab63751df5836f3bec9de7416f0b7c
bonnet <theo.bonnet1@etu.unice.fr>
2019-04-03 12:50:18 +0200

    REST_APP with a bug
    I have trouble launching it but it is better thant what we have

--- TD/REST_APP/REST_APP/Service1.cs
+++ TD/REST_APP/REST_APP/Service1.cs
@@ -1,31 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
+using System.ServiceModel.Web;
 
 namespace REST_APP
 {
-    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
     public class Service1 : IService1
     {
-        public string GetData(int value)
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json,
+       UriTemplate = "data/{id}")]
+        public Person GetData(string id)
         {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
+            // lookup person with the requested id
+            return new Person()
             {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+                Id = Convert.ToInt32(id),
+                Name = "Leo Messi"
+            };
         }
     }
-}
+    }
